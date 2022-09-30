772e352c6b643cb3a8c393430e7dba7715de002f
marzookh <farook@ualberta.ca>
2022-09-29 23:09:06 -0600

    Final commit

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -32,7 +33,64 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        received_data = self.data.decode('utf-8')
+
+        # return status code of 405 for any method we cannot handle
+        if 'POST' in received_data or 'PUT' in received_data or 'DELETE' in received_data:
+            send_data = "HTTP/1.1 405 Method Not Allowed\r\n\r\n"
+            self.request.sendall(send_data.encode())
+            return
+
+
+        # returns status code of 301 after checking file path
+        file_content = ''
+        response_data = ''
+        local_path = received_data.splitlines()[0].split()[1]
+        current_local_path = os.getcwd() + '/www' + local_path
+
+        if (local_path.endswith('/')):
+            file_path = os.path.abspath(current_local_path + 'index.html')
+        else:
+            file_path = os.path.abspath(current_local_path)
+        
+
+        resource_path = os.path.exists(os.path.abspath(current_local_path))
+
+        if (not os.path.isfile(file_path) and resource_path):
+            response_data = "HTTP/1.1 301 Moved Permenantly\r\n"
+            response_data += "Location: http://127.0.0.1:8080{}/\r\n".format(local_path)
+            response_data += "Content-Type: text/html; charset=utf-8\r\n"
+
+        # returns status code of 404
+        elif(os.getcwd() not in file_path):
+            response_data = "HTTP/1.1 404 Not Found\r\n"
+
+        
+        else:
+            if os.path.exists(file_path):
+                with open(file_path,'r') as file:
+                    print('file exists')
+                    file_content = file.read()
+                    response_data = "HTTP/1.1 200 OK\r\n"
+
+                    # check mime type
+                    if file_path.endswith(".css"):
+                        mime_type = 'text/css'
+                    else:
+                        mime_type = 'text/html'
+                    
+                    response_data += 'Content-Type: '+ mime_type+ '; charset=utf-8\r\n'
+            
+            else:
+                response_data = "HTTP/1.1 404 Not Found\r\n"
+
+            response_data += "Content-Length: {}\r\n\r\n".format(len(file_content))
+            response_data += file_content
+        
+        self.request.sendall(response_data.encode())
+        self.request.close()
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -40,7 +98,7 @@ if __name__ == "__main__":
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
-
+    
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
